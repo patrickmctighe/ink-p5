ef1b9352bff0b8c7e138c0300cd9ce1ec5e8a1ef
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2024-05-16 11:27:31 -0400

    tine-effect

--- ink.js
+++ ink.js
@@ -1,9 +1,8 @@
-const circleDetail = 100;
+const circleDetail = 400;
 
 class Drop{
     constructor(x,y,r){
-        this.x = x;
-        this.y = y;
+      this.center = createVector(x,y);
         this.r = r;
 
         this.vertices = [];
@@ -11,13 +10,38 @@ class Drop{
             let angle = map(i,0 ,circleDetail, 0 , TWO_PI)
             let v = createVector(cos(angle), sin(angle));
             v.mult(this.r);
-            v.add(this.x, this.y);
+            v.add(this.center);
             this.vertices[i]=v;
         }
+        this.col =random(0,200)
     }
 
+    tine(x,z,c){
+        let u = 1/pow(2,1/c);
+        for(let v of this.vertices){
+            v.x = v.x
+            v.y = v.y+z*pow(u,abs(v.x-x))
+        }
+    }
+        marble(other){
+            for(let v of this.vertices){
+                let c= other.center;
+                let r = other.r;
+                let p =v.copy();
+                p.sub(c);
+                let m = p.mag();
+                let root = sqrt(1 + (r*r)/(m*m));
+                p.mult(root);
+                p.add(c);
+                v.set(p);
+               
+            }
+        }
+    
+
     show(){
-        fill(0);
+        fill(this.col);
+        noStroke();
      beginShape();
         for(let v of this.vertices){
             vertex(v.x, v.y);
--- sketch.js
+++ sketch.js
@@ -1,19 +1,37 @@
 let drops = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  for(let i = 0; i<50;i++){ addInk(random(width),random(height),50);}
+ 
+  
 }
 
-function mousePressed() {
-let drop = new Drop(mouseX, mouseY, 50);
+function tineline(xl,z,c){
+for(let drop of drops){
+  drop.tine(xl,z,c)
+}
+}
+
+
+
+function addInk(x,y,r){
+  let drop = new Drop(x,y,r);
 
 for(let other of drops){
-  drop.marble(other);
+  other.marble(drop);
 }
 drops.push(drop);
 }
 
 function draw() {
+  // let x = random(width);
+  // let y = random(height);
+  // let r = random(10, 10);
+  // addInk(x,y,r);
+  if(mouseIsPressed){
+    tineline(mouseX,4,8)
+  }
   background(220);
   for (let drop of drops){
     drop.show();
